354a459b3c50fc0bb9186f462aee6b752fec8074
JunJie <tjunjie2000@gmail.com>
2022-06-10 20:18:41 +0800

    init firebase

--- navigation/MyTabs.js
+++ navigation/MyTabs.js
@@ -1,6 +1,8 @@
 import HomeScreen from "../components/screens/HomeScreen";
 import ChatScreen from "../components/screens/ChatScreen";
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Login from "../components/screens/Login";
+import Signup from "../components/screens/Signup";
 
 const Tab = createBottomTabNavigator();
 
@@ -9,6 +11,9 @@ function MyTabs() {
       <Tab.Navigator>
         <Tab.Screen name="Home" component={HomeScreen} />
         <Tab.Screen name="Chat" component={ChatScreen} />
+        <Tab.Screen name="Login" component={Login} />
+        <Tab.Screen name="SignUp" component={Signup} />
+        
       </Tab.Navigator>
     );
   }
