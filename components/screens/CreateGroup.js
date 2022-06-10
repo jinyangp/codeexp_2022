c105c1998d3d4efaac3151aaa563b6f14d9a062e
Evande1 <crossjab@gmail.com>
2022-06-10 19:50:36 +0800

    'switching-com'

---
+++ components/screens/CreateGroup.js
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+function CreateGroup() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Chat!</Text>
+      </View>
+    );
+  }
+
+export default CreateGroup;
